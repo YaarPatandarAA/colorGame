fd772b91f5a1d37eac6a3b5685c5cd5f240621d6
Amarjit Singh <b.singh.amarjit@gmail.com>
2019-01-03 21:32:38 -0800

    make webpage look better

--- colorGame.js
+++ colorGame.js
@@ -1,3 +1,4 @@
+var numSquares = 6;
 var colors = generateRandomColors(6);
 var squares = document.querySelectorAll(".squares");
 var pickedColor = pickColor();
@@ -5,15 +6,48 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-resetButton.addEventListener("click", function() {
-    colors = generateRandomColors(6);
+
+easyBtn.addEventListener("click", function () {
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected")
+    numSquares = 3;
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    for (let i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.backgroundColor = colors[i];
+        }
+        else {
+            squares[i].style.display = "none";
+        }
+    }
+})
+hardBtn.addEventListener("click", function () {
+    easyBtn.classList.remove("selected");
+    hardBtn.classList.add("selected")
+    numSquares = 6
+    colors = generateRandomColors(numSquares);
     pickedColor = pickColor();
     colorDisplay.textContent = pickedColor;
     for (let i = 0; i < squares.length; i++) {
         squares[i].style.backgroundColor = colors[i];
+        squares[i].style.display = "block";
     }
-    h1.style.backgroundColor = "#232323"
+})
+
+resetButton.addEventListener("click", function () {
+    colors = generateRandomColors(numSquares);
+    pickedColor = pickColor();
+    colorDisplay.textContent = pickedColor;
+    messageDisplay.textContent = "";
+    for (let i = 0; i < squares.length; i++) {
+        squares[i].style.backgroundColor = colors[i];
+    }
+    h1.style.backgroundColor = "steelblue"
     this.textContent = "New Colors"
 });
 
